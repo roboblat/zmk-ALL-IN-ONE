3708631e8343b77a332d10e34ec5b7aa05b8ad96
roboblat <roboblat@gmail.com>
2026-06-03 01:30:46 -0400

    Update layer_status.h

--- boards/shields/dongle_screen/src/widgets/layer_status.h
+++ boards/shields/dongle_screen/src/widgets/layer_status.h
@@ -11,7 +11,9 @@
 
 struct zmk_widget_layer_status {
     sys_snode_t node;
-    lv_obj_t *obj;
+    lv_obj_t *obj;            /* container; aligned by the status screen */
+    lv_obj_t *outline[8];     /* black copies, offset around the center */
+    lv_obj_t *center;         /* white label on top */
 };
 
 int zmk_widget_layer_status_init(struct zmk_widget_layer_status *widget, lv_obj_t *parent);
